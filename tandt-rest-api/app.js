b0e4cd536f2c03d7bfe7f356220d33eb80f3419b
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2019-11-18 14:17:55 +0530

    Update app.js

--- tandt-rest-api/app.js
+++ tandt-rest-api/app.js
@@ -42,7 +42,7 @@ var blockListener = require('./blocklistener.js');
 
 hfc.addConfigFile('config.json');
 var host = 'localhost';
-var port = 3001;
+var port = 8080;
 var username = "";
 var orgName = "";
 var channelName = hfc.getConfigSetting('channelName');
