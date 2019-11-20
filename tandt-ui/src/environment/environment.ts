5d3c0e2cbfe62ed51bd19d36df7235892f260803
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2019-11-20 11:50:44 +0530

    Update environment.ts

--- tandt-ui/src/environment/environment.ts
+++ tandt-ui/src/environment/environment.ts
@@ -4,7 +4,7 @@ export const environment={
 	port:'',
 	dbhost:'',
 	dbport:'',
-	api_url:'http://trackandt-Blockcha-GOV2BHTGCFDU-2122750619.us-east-1.elb.amazonaws.com/',
+	api_url:'http://trackandt-Blockcha-1PRH9DF9BGAJU-674473020.us-east-1.elb.amazonaws.com/',
 	test:'test',
-	socket_url:'ws://trackandt-Blockcha-GOV2BHTGCFDU-2122750619.us-east-1.elb.amazonaws.com'
+	socket_url:'ws://trackandt-Blockcha-1PRH9DF9BGAJU-674473020.us-east-1.elb.amazonaws.com'
 };
