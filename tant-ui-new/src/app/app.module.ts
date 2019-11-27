ac0f2a704bde1ef1c52040c21f21a76ac5e9cf48
Shashank M G <shashank.mg@BRILLIO.COM>
2019-11-27 10:31:39 +0530

    added manufacturers, login

--- tant-ui-new/src/app/app.module.ts
+++ tant-ui-new/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { DonateComponent } from './ui/donate/donate.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgosListComponent } from './ui/ngos-list/ngos-list.component';
 import { NgoDetailsComponent } from './ui/ngo-details/ngo-details.component';
+import { ManufacturerComponent } from '.ui/manufacturerlist/manufacturers-list.component';
 
 // Custom Componants
 import { RatingComponent, RatingService } from './ui/components/rating/rating.component';
---
+++ tant-ui-new/src/app/ui/manufacturers-list/manufacturers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewContainerRef, ViewChild, ComponentFactoryResolver, HostListener } from '@angular/core';
+import { Ngo, Project, Rating } from 'src/app/models';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { SessionService, UtilsService } from 'src/app/services/shared';
+import { DonateService, DashboardService, NgoService } from 'src/app/services';
+
+@Component({
+  selector: 'app-manufacturers-list',
+  templateUrl: './manufacturers-list.component.html',
+  styleUrls: ['./manufacturers-list.component.scss']
+})
+
+export class ManufacturerListComponent implements OnInit {
+
+  @ViewChild('graphcontainer', { read: ViewContainerRef }) viewContainer: ViewContainerRef;
+
+  completeBatchList=[];
