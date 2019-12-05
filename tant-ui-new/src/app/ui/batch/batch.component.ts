b426203fe886df28f8b17d7cf8b135894975e44c
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2019-12-05 14:38:46 +0530

    Add files via upload

---
+++ tant-ui-new/src/app/ui/batch/batch.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DonorService } from 'src/app/services/shared';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-batch',
+  templateUrl: './batch.component.html',
+  styleUrls: ['./batch.component.scss']
+})
+export class BatchListComponent implements OnInit {
+  batchForm: FormGroup;
+  submitted = false;
+  error: string = null;
+  loading = false;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: DonorService) {
+  }
+
+  ngOnInit() {
+    this.batchForm = this.formBuilder.group({
+      BatchId: new FormControl('', {
+        validators: Validators.compose([Validators.required,
+        Validators.minLength(4), Validators.maxLength(20)]),
+        updateOn: 'blur'
+      }),
+      BatchName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ]))
+    });
+  }
+
+  get user() { return this.batchForm.controls; }
+
+  batch() {
+    if (this.loading) {
+      return;
+    }
+    this.loading = true;
+    this.submitted = true;
+    if (this.batchForm.invalid) {
+      this.loading = false;
+      return;
+    }
+    const user = this.batchForm.value;
+    this.userService.createBatch(user).subscribe(
+      data => {
+        if (data.success) {
+        
+            resp => {
+              this.router.navigate(['ngolist']);
+              return;
+            },
+            err => {
+              this.loading = false;
+              this.error = err.statusText;
+            }
+          
+        } else {
+          this.loading = false;
+          this.error = 'Batch Id already in use!';
+        }
+      },
+      err => {
+        this.loading = false;
+        this.error = err.statusText + ". Ensure you are using HTTP, not HTTPS, to access the site.";
+      }
+    );
+  }
+
+}
---
+++ tant-ui-new/src/app/ui/order/order.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DonorService } from 'src/app/services/shared';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss']
+})
+export class OrderListComponent implements OnInit {
+  orderForm: FormGroup;
+  submitted = false;
+  error: string = null;
+  loading = false;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: DonorService) {
+  }
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      OrderId: new FormControl('', {
+        validators: Validators.compose([Validators.required,
+        Validators.minLength(4), Validators.maxLength(20)]),
+        updateOn: 'blur'
+      }),
+      OredrName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])),
+      Quantity: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.Quantity
+      ]))
+
+    });
+  }
+
+  get user() { return this.orderForm.controls; }
+
+  signup() {
+    if (this.loading) {
+      return;
+    }
+    this.loading = true;
+    this.submitted = true;
+    if (this.orderForm.invalid) {
+      this.loading = false;
+      return;
+    }
+    const user = this.orderForm.value;
+    this.userService.createOrder(user).subscribe(
+      data => {
+        if (data.success) {
+        
+            resp => {
+              this.router.navigate(['ngolist']);
+              return;
+            },
+            err => {
+              this.loading = false;
+              this.error = err.statusText;
+            }
+          
+        } else {
+          this.loading = false;
+          this.error = 'Order Id already in use!';
+        }
+      },
+      err => {
+        this.loading = false;
+        this.error = err.statusText + ". Ensure you are using HTTP, not HTTPS, to access the site.";
+      }
+    );
+  }
+
+}
