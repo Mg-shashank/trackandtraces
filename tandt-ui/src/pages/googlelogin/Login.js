3b2740979668e3b961dfbf0753c3b453169ce087
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2019-12-17 12:21:35 +0530

    Add files via upload
    Google Login

---
+++ tandt-ui/src/pages/googlelogin/Login.js
@@ -0,0 +1,90 @@
+/* global gapi */
+import React, {Component} from 'react';
+import './login.scss';
+import LandingPage from '../dashboard/dashboard';
+//import { GoogleLogin } from 'react-google-login';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignedIn: false
+    }
+  }
+
+  componentDidMount() {
+    const successCallback = this.onSuccess.bind(this);
+    const failureCallback = this.onLoginFailed.bind(this);
+    window.gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '252338212303-eku5e56140e59uhvqeq36fl8neetega2.apps.googleusercontent.com',
+      })
+
+      // this.auth2.attachClickHandler(document.querySelector('#loginButton'), {}, this.onLoginSuccessful.bind(this))
+
+      this.auth2.then(() => {
+        console.log('on init');
+        this.setState({
+          isSignedIn: this.auth2.isSignedIn.get(),
+        });
+      });
+    });
+
+    window.gapi.load('signin2', function() {
+      // Method 3: render a sign in button
+      // using this method will show Signed In if the user is already signed in
+      var opts = {
+        width: 200,
+        height: 50,
+        client_id: '252338212303-eku5e56140e59uhvqeq36fl8neetega2.apps.googleusercontent.com',
+        onSuccess: successCallback,
+        onFailure: failureCallback
+      }
+      gapi.signin2.render('loginButton', opts)
+    })
+  }
+
+  onSuccess() {
+    console.log('on success')
+    this.setState({
+      isSignedIn: true,
+      err: null
+    })
+  }
+
+ onLoginFailed(err) {
+    console.log('on failure')
+    this.setState({
+      isSignedIn: false
+    })
+  }
+
+  getContent() {
+    if (this.state.isSignedIn) {
+    //  return <Switch> <Route path="/mem" render={() => ( <FailedComponent logout={this.onLoginFailed} />)} /> </Switch>
+    //  return <FailedComponent logout={this.onLoginFailed}/>
+        return <LandingPage/>
+      } else {
+      return (
+        <div>
+          <p style={{fontSize:15}}>You are not signed in. Click here to sign in.</p>
+          <button id="loginButton">Login with Google</button>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2 style={{fontFamily:" Helvetica, sans-serif"}}>Track And Trace</h2>
+          {this.getContent()}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Login;
