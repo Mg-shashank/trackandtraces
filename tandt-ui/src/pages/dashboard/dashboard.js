6da93e4ce9c95b133deca52b63aae296114d9cf6
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2019-12-18 13:05:48 +0530

    Update dashboard.js

--- tandt-ui/src/pages/dashboard/dashboard.js
+++ tandt-ui/src/pages/dashboard/dashboard.js
@@ -5,8 +5,10 @@ import {Button, MenuItem, Menu} from "@material-ui/core";
 import logo from "./images/brillio-logo.png";
 
 import usericon from "./images/user-icon.svg";
-import dots from "./images/dots.svg"
-import router from "./images/router.png"
+import dots from "./images/dots.svg";
+import router from "./images/router.png";
+import router2 from "./images/router2.jpg";
+import router3 from "./images/router3.jpg"
 import "./dashboard.scss";
 
 
@@ -71,23 +73,15 @@ import "./dashboard.scss";
                       <div className="col-lg-3 col-mt-3  float-right  recent-activities">
               <div className="col-lg-12 col-mt-12 col-mt-5 padding0">
                 <h3 className="section-header">Recent Activities</h3>
-                <div className="activity-card">
-                  <p className="act-head">New Order</p>
-                  <p className="act-content">You have received a new order for Linksys AC1200 Max WiFi Range Extender RE6500 from Abbott Group Enterprise.</p>
-                  <p className="text-right"><Link to="/traceorder"><div className="view-more">View more</div></Link></p>
-                </div>
+                
                 <div className="activity-card">
                   <p className="act-head">Order Accepted</p>
                   <p className="act-content">Order accepted for for Netgear Nightwalk X10 AD7200 from Hirthe Group Enterprise.</p>
                   <p className="text-right"><Link to="/traceorder"><div className="view-more">View more</div></Link></p>
                 </div>
+               
                 <div className="activity-card">
-                  <p className="act-head">New Order</p>
-                  <p className="act-content">You have received a new order for D-Link AC2600 (DIR-2680) from Schuster Ltd Enterprise.</p>
-                  <p className="text-right"><Link to="/traceorder"><div className="view-more">View more</div></Link></p>
-                </div>
-                <div className="activity-card">
-                  <p className="act-head">Order Received</p>
+                  <p className="act-head">Order Accepted</p>
                   <p className="act-content">Order received for D-Link DWR-2010 5G Router from Schuster Ltd Enterprise.</p>
                   <p className="text-right"><Link to="/traceorder"><div className="view-more">View more</div></Link></p>
                 </div>
@@ -118,10 +112,11 @@ import "./dashboard.scss";
                         <img src={router} alt="s1" className="s1" />
                         <h3 className="title">Asus ROG Rapture GT-AC5300</h3>
                         <p className="sub-title">Router</p>
-                        <p className="card-label"><label>Device category:</label> Network</p>
+                        <p className="card-label"><label>End of Warranty:</label> March</p>
                         <p className="attribute">MAC Address: D4:05:15:35:4A:11</p>
-                        <p className="attribute">Serial number: 12AC5600214</p>
-                        <p className="attribute">Domain name: Telecom</p>
+                        <p className="attribute">IPv6 Compatible: yes</p>
+                        <p className="attribute">Data Transfer Rate: 1200 Mbps</p>
+<p className="attribute">Frequency Band : Dual Band</p>
                       </div>
                     </div>
                     <div className="col-lg-4 col-md-4">
@@ -137,18 +132,20 @@ import "./dashboard.scss";
                               open={Boolean(anchorEl)}
                               onClose={handleClose}
                             >
-                              <MenuItem onClick={handleClose}>Device Model: AC5300</MenuItem>
-                              <MenuItem onClick={handleClose}>evice Type: Router</MenuItem>
-                              <MenuItem onClick={handleClose}>Device Vendor: Asus</MenuItem>
+                              <MenuItem onClick={handleClose}>Device Model: AC2600</MenuItem>
+                              <MenuItem onClick={handleClose}>Device Type: Router</MenuItem>
+                              <MenuItem onClick={handleClose}>Device Vendor: Abbot</MenuItem>
                             </Menu>                             
                         </div>
-                        <img src={router} alt="s1" className="s1" />
-                        <h3 className="title">Asus ROG Rapture GT-AC5300</h3>
+                        <img src={router2} alt="s1" className="s1" />
+                        <h3 className="title">D-Link AC2600 (DIR-2680)</h3>
                         <p className="sub-title">Router</p>
-                        <p className="card-label"><label>Device category:</label> Network</p>
-                        <p className="attribute">MAC Address: D4:05:15:35:4A:11</p>
-                        <p className="attribute">Serial number: 12AC5600214</p>
-                        <p className="attribute">Domain name: Telecom</p>
+                        <p className="card-label"><label>End of Warranty:</label> June</p>
+                        <p className="attribute">MAC Address: D2:65:75:87:A7:05</p>
+                     
+                        <p className="attribute">IPv6 Compatible: yes</p>
+                        <p className="attribute">Data Transfer Rate: 3600 Mbps</p>
+<p className="attribute">Frequency Band : TriBand</p>
                       </div>
                     </div>
 
@@ -165,18 +162,20 @@ import "./dashboard.scss";
                               open={Boolean(anchorEl)}
                               onClose={handleClose}
                             >
-                              <MenuItem onClick={handleClose}>Device Model: AC5300</MenuItem>
+                              <MenuItem onClick={handleClose}>Device Model: AC66U</MenuItem>
                               <MenuItem onClick={handleClose}>Device Type: Router</MenuItem>
                               <MenuItem onClick={handleClose}>Device Vendor: Asus</MenuItem>
                             </Menu>
                         </div>
-                      <img src={router} alt="s1" className="s1" />
-                      <h3 className="title">Asus ROG Rapture GT-AC5300</h3>
+                      <img src={router3} alt="s1" className="s1" />
+                      <h3 className="title">Asus RT-AC66U B1</h3>
                       <p className="sub-title">Router</p>
-                      <p className="card-label"><label>Device category:</label> Network</p>
-                      <p className="attribute">MAC Address: D4:05:15:35:4A:11</p>
-                      <p className="attribute">Serial number: 12AC5600214</p>
-                      <p className="attribute">Domain name: Telecom</p>
+                      <p className="card-label"><label>End of Warranty:</label> June</p>
+                      <p className="attribute">MAC Address: A4:02:33:93:7A:83</p>
+                 
+                        <p className="attribute">IPv6 Compatible: yes</p>
+                        <p className="attribute">Data Transfer Rate: 4600 Mbps</p>
+<p className="attribute">Frequency Band : Dual Band</p>
                     </div>
                   </div>
                 </div>
