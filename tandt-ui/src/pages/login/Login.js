5f4dd2f976c10506e26cff0b66c2d89da583806c
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2019-12-26 06:12:47 +0530

    Update Login.js

--- tandt-ui/src/pages/login/Login.js
+++ tandt-ui/src/pages/login/Login.js
@@ -6,9 +6,13 @@ import LandingPage from '../dashboard/dashboard';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import {Button, MenuItem, Menu} from "@material-ui/core";
 import './blogo.png';
-import brillio from './brillio.jpg';
+import brillio from './brillio.svg';
+import brillio1 from './brillio1.png';
+import login from './login.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LogButton from '../buttoncomponent/LogButton';
-
+import image1 from './image1.png';
+import './site.scss';
 const tstyle = {
   width:500,
   height:380,
@@ -16,6 +20,14 @@ const tstyle = {
   top:250,
   left:200
 };
+
+ const introimg = {
+   height:'100vh',
+   width : '58vw',
+  //backgroundImage : "url(" + { image1 } + ")",
+ 	backgroundRepeat: 'no-repeat',
+ 	backgroundSize: 'cover'
+  }
 
 class Login extends Component {
   constructor(props) {
@@ -92,8 +104,8 @@ class Login extends Component {
       } else {
       return (
         <div>
-          <p style={{fontSize:30}}>SignIn to Track and Trace</p>
-          <button id="loginButton" style={{left:50}} className="button1">Login with Google</button>
+        {/*<button type="button" id="loginButton" className="btn btn-primary" data-toggle="modal" data-target="#login-type">Login</button>*/}
+          <button id="loginButton" className="button1">Login with Google</button>
         </div>
       )
     }
@@ -101,18 +113,31 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <div class="row">
-    <div style={tstyle} class="col-md-5">
-      <div>
-        <img src={brillio} alt="logo" />
-    </div>
-    </div>
-    <div class="col-md-5" style={tstyle} >{this.getContent()}</div>
-  </div>
-</div>
+      <div className="container-fluid padding0">
+    		<div className="col-lg-7 col-md-7 padding0 introimg">
+    			{/*<img src={brillio} alt="img" className=""/>*/}
+          <img src={brillio1} alt='image1' style={introimg}/>
+          </div>
+    		<div className="col-lg-5 col-md-5 login-container">
+    			<p className="text-center"><img src={login} alt="login" /></p>
+    			<div class="text-center">
+            <div class="col-md-5">{this.getContent()}</div>
+          </div>
+    		</div>
+    	</div>
     );
   }
 }
 
 export default Login;
+
+{/*<div>
+  <div class="row">
+   <div style={tstyle} class="col-md-5">
+    <div>
+     <img src={brillio} alt="logo" />
+    </div>
+   </div>
+   <div class="col-md-5" style={tstyle} >{this.getContent()}</div>
+ </div>
+</div>*/}
