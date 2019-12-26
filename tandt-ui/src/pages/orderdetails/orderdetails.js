fc48879ae60f0b70e5ed33eb3c76b96740680f35
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2019-12-26 10:45:58 +0530

    Update orderdetails.js

--- tandt-ui/src/pages/orderdetails/orderdetails.js
+++ tandt-ui/src/pages/orderdetails/orderdetails.js
@@ -8,20 +8,18 @@ import rect from "./images/rect.svg"
 import router from "./images/router.png"
 import "./dashboard.scss";
 // import { ReceiptTwoTone } from "@material-ui/icons";
-
 // import { useUserDispatch, loginUser } from "../../context/UserContext";
 
-    function Landingpage(props) {    
-      return( 
+    function Landingpage(props) {
+      return(
         <div class="container-fluid padding0">
         <header>
-              <span className="logo"><img className="logoImage" src={logo} alt="Brillio logo" width="125px"/></span>
-            
-              <div className="userBlock collapse navbar-collapse">
-			  <Link to="/help">Help</Link>&nbsp;<span className="pipe">|</span>&nbsp;<a href="#"> <img src={usericon} alt="user" /></a>
-          		</div>  
-             
-          </header>
+          <span className="logo"><img className="logoImage" src={logo} alt="Brillio logo" width="125px"/>
+          </span>
+          <div className="userBlock collapse navbar-collapse">
+			  <Link to="/help">Help</Link>&nbsp;<span className="pipe">|</span>&nbsp;<a href="#">
+          <img src={usericon} alt="user" /></a></div>
+        </header>
 		  <section class="content">
 			<h3 class="section-header">Order Details</h3>
 			<h6><div class="col-lg-12 col-md-12 place-order">
@@ -36,14 +34,14 @@ import "./dashboard.scss";
 					<p class="network-device">Billing Id<br/><span>813725866655</span></p>
 				</div>
 				<div class="col-lg-2 col-md-2">
-					
+
 				</div>
 				<div class="col-lg-12 col-md-12 padding0">
 					<div class="col-lg-3 col-md-3">
 						<label class="form-label">Vendor Name</label>
 						<p>Abbott Group Enterprise</p>
 					</div>
-				
+
 					<div class="col-lg-3 col-md-3">
 						<label class="form-label">Quantity</label>
 						<p>10000</p>
@@ -56,24 +54,22 @@ import "./dashboard.scss";
 				<div class="col-lg-12 col-md-12">
 					<label class="form-label">Addition Requests</label>
 					<p>Condition:Refurbished Status: Working</p>
-                    <p>Upgrade device compatible to 5G.</p>
+          <p>Upgrade device compatible to 5G.</p>
 				</div>
-				
-				<div class="col-lg-12 col-md-12 text-right">
-				<Link to="/createorder"><div class="btn btn-cancel">Cancel</div></Link>
-					<Link to="/trackorder"><div class="btn btn-prim">Track Order</div></Link>
-
-			
-					
+        <div>
+        <div class="col-sm-12 text-right">
+          <button class="btn btn-danger">Reject Order</button>&nbsp;&nbsp;
+    		  <button class="btn btn-primary">Accept Order</button>
+        </div>
+				<div class="col-md-4 text-left">
+				  <Link to="/createorder"><button class="btn btn-small btn-cancel">Cancel</button></Link>&nbsp;&nbsp;
+					<Link to="/trackorder"><button class="btn btn-primary">Track Order</button></Link>
 				</div>
-			</div></h6>
+        </div>
+      </div>
+    </h6>
 		</section>
- 
-        </div>  
-       
-         
-);
+  </div>
+  );
 }
-
-
 export default withRouter(Landingpage);
