2587d88534abf0b45afd857d23cce096e3acfec5
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-01-02 07:05:23 +0530

    roles and emails

--- tandt-ui/src/pages/login/Login.js
+++ tandt-ui/src/pages/login/Login.js
@@ -14,6 +14,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import LogButton from '../buttoncomponent/LogButton';
 import image1 from './image1.png';
 import './site.scss';
+//import {roles} from '../../config';
+import orderdetails from '../orderdetails/orderdetails';
+import Unknown from './Unknown';
+
 const tstyle = {
   width:500,
   height:380,
@@ -25,11 +29,25 @@ const tstyle = {
  const introimg = {
    height:'100vh',
    width : '58vw',
-  //backgroundImage : "url(" + { image1 } + ")",
- 	backgroundRepeat: 'no-repeat',
+  backgroundRepeat: 'no-repeat',
  	backgroundSize: 'cover'
   }
+  const emailIds = ['shashank30051997@gmail.com', 'abcd1@gmail.com', 'abcd@gmail.com']
+  const roles = [{
+                    "role": "serviceProvider",
+                    "emailId":"shashank30051997@gmail.com"
+                },
+                {
+                    "role": "distributor",
+                    "emailId":"abcd1//@gmail.com"
+                },
+                {
+                    "role": "manuacturer",
+                    "emailId":"abcd3@gmail.com"
+                }];
 
+ var email;
+ let signout;
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -37,25 +55,43 @@ class Login extends Component {
       isSignedIn: false
     }
   }
-
+  
   componentDidMount() {
     const successCallback = this.onSuccess.bind(this);
     const failureCallback = this.onLoginFailed.bind(this);
     window.gapi.load('auth2', () => {
+
       this.auth2 = gapi.auth2.init({
         client_id: '252338212303-eku5e56140e59uhvqeq36fl8neetega2.apps.googleusercontent.com',
-      })
+        fetch_basic_profile: false,
+        scope: 'profile'
+      })     
 
-      // this.auth2.attachClickHandler(document.querySelector('#loginButton'), {}, this.onLoginSuccessful.bind(this))
+     /* this.auth2.signIn().then(function() {
+          console.log(this.auth2.currentUser.get().getId());
+        }); */
 
       this.auth2.then(() => {
-        console.log('on init');
-        this.setState({
-          isSignedIn: this.auth2.isSignedIn.get(),
-        });
-      });
-    });
-
+        console.log('on init'); 
+        console.log(this.state.isSignedIn);
+         this.setState({ isSignedIn: this.auth2.isSignedIn.get(),});              
+      });  
+      
+      if (this.auth2.isSignedIn.get()) {
+        console.log("Hello")
+        var profile = this.auth2.currentUser.get().getBasicProfile();
+        //console.log('ID: ' + profile.getId());
+        //console.log('Full Name: ' + profile.getName());
+        //console.log('Given Name: ' + profile.getGivenName());
+        //console.log('Family Name: ' + profile.getFamilyName());
+        //console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        email = profile.getEmail();
+        console.log(email);
+        
+      }
+     });
+    
     window.gapi.load('signin2', function() {
         var opts = {
         width: 200,
@@ -64,13 +100,13 @@ class Login extends Component {
         onSuccess: successCallback,
         onFailure: failureCallback
       }
-      gapi.signin2.render('loginButton', opts)
+      gapi.signin2.render('loginButton', opts)      
     })
   }
 
-  onSuccess() {
-    console.log('on success')
-    this.setState({
+   onSuccess() {
+    console.log('on success')    
+     this.setState({
       isSignedIn: true,
       err: null
     })
@@ -83,40 +119,57 @@ class Login extends Component {
     })
   }
 
+  getRoleByEmailId(emailid) {
+    return roles.filter(function(roles){return (roles.emailId == emailid)});
+  }
+ 
+  
   getContent() {
-    if (this.state.isSignedIn) {
-    //  return <Switch> <Route path="/mem" render={() => ( <FailedComponent logout={this.onLoginFailed} />)} /> </Switch>
-    //  return <FailedComponent logout={this.onLoginFailed}/>
+    console.log(email)
+    var loggedInUsersEmailId = 'shashank30051997@gmail.com';
+    //var loggedInUsersEmailId = email;
+
+
+    if (this.state.isSignedIn) { 
+       //if(emailIds[loggedInUsersEmailId]){
+        var roleData = this.getRoleByEmailId(loggedInUsersEmailId);
+    
+        console.log(roleData[0].role);
+        
+        localStorage.setItem('logged-in-user-email-id', loggedInUsersEmailId);
+        
+    
+        if(roleData[0].role){
         return(
-          <div className="wrapper">
-          {/* <header>
-                <div className="userBlock collapse navbar-collapse">
-                <Link to="/help">Help</Link>&nbsp;
-                </div>
-            </header>
-            <section>*/ }
-        <div className="col-lg-12 col-md-12">
-         {/*<Link to="/dashboard"><div className="btn btn-prim pull-right">Go to Dashboard</div></Link>*/}
-         <Redirect to='/dashboard'/>
-        </div>
-            {/*</section>*/}
-        </div>
-  );
-      } else {
+        <div className="wrapper">          
+          <div className="col-lg-12 col-md-12">        
+            <Redirect to='/dashboard'/>
+            {/*<button onClick={this.Clicked()}>Signout</button>*/}
+          </div>           
+        </div>      
+      );
+    } else {
       return (
-        <div>
-        {/*<button type="button" id="loginButton" className="btn btn-primary" data-toggle="modal" data-target="#login-type">Login</button>*/}
+        <div className="wrapper">          
+       <div className="col-lg-12 col-md-12">        
+       <Redirect to='/Unknown'/>
+       </div>           
+       </div>);
+    }
+  } else {
+      return (
+        <div>        
           <button id="loginButton" className="button1">Login with Google</button>
         </div>
-      )
-    }
-  }
+      );
+    }        
+}
+
 
   render() {
     return (
       <div className="container-fluid padding0">
-    		<div className="col-lg-7 col-md-7 padding0 introimg">
-    			{/*<img src={brillio} alt="img" className=""/>*/}
+    		<div className="col-lg-7 col-md-7 padding0 introimg">    			
           <img src={brillio1} alt='image1' style={introimg}/>
           </div>
     		<div className="col-lg-5 col-md-5 login-container">
@@ -131,14 +184,3 @@ class Login extends Component {
 }
 
 export default Login;
-
-{/*<div>
-  <div class="row">
-   <div style={tstyle} class="col-md-5">
-    <div>
-     <img src={brillio} alt="logo" />
-    </div>
-   </div>
-   <div class="col-md-5" style={tstyle} >{this.getContent()}</div>
- </div>
-</div>*/}
