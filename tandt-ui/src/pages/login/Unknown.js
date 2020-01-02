fd466592a1f4809f9b4964c0864035a545c46992
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-01-02 07:06:42 +0530

    Add files via upload

---
+++ tandt-ui/src/pages/login/Unknown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Unknown extends React.Component{
+    render(){
+        return(
+            <div><h2> 404 Error Page Not Found  </h2></div>
+        )
+    }
+}
