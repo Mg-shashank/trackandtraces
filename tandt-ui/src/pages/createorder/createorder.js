d71e5c4b5e9c5618f186c3c432bf4ee65393e5c0
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2020-01-16 14:03:12 +0530

    Update createorder.js

--- tandt-ui/src/pages/createorder/createorder.js
+++ tandt-ui/src/pages/createorder/createorder.js
@@ -1,62 +1,80 @@
 import React, {useState, Component }  from "react";
 import { withRouter,Link } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import logo from "./images/brillio-logo.png";
 import usericon from "./images/user-icon.svg";
 import router from "./images/router.png";
 import {GoogleLogin,GoogleLogout} from 'react-google-login';
 import "./dashboard.scss";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import $ from 'jquery';
 var image=localStorage.getItem('profile-picture');
 var name=localStorage.getItem('name');
+var routers=localStorage.getItem('router');
 class Landingpage extends React.Component {
 	constructor(props){
 	  super(props);
-		this.toggle= this.toggle.bind(this);
-	  this.state = {		
-		addresss: '',
-		  dropdownOpen:false
+	  this.state = {
+		
+		address: '',
+        vendor:'',
+        address1:'',
+        quantity:'',
 	  };
   
 	  this._handleSubmit = this._handleSubmit.bind(this);
 	  this._handleChange = this._handleChange.bind(this);
-	  this._handleChangeMsg = this._handleChangeMsg.bind(this);
+      this._handleChangeMsg = this._handleChangeMsg.bind(this);
+      this._handleChangename = this._handleChangename.bind(this);
+      this._handleChangeq = this._handleChangeq.bind(this);
 	}
   
 	// Change state of input field so text is updated while typing
-	toggle=()=>{
-		this.setState((prevState)=>{
-			return{dropdownOpen:!prevState.dropdownOpen};
-		});
-	}
 	_handleChange(e) {
-	  this.setState({
-		contactEmail: e.target.value,
-	  });
-	}
-	// Change state of input field so text is updated while typing
-	_handleChangeMsg(e) {
-	  this.setState({
-		addresss: e.target.value
-	  });
-	}
+		this.setState({
+		  address1: e.target.value,
+		});
+	  }
+	  _handleChangeq(e) {
+		  this.setState({
+			quantity: e.target.value,
+		  });
+		}
+	  _handleChangename(e) {
+		  this.setState({
+			  vendor: e.target.value,
+		  });
+		}
+	  _handleChangeMsg(e) {
+		this.setState({
+		  address: e.target.value,
+		  
+		
+		});
+	  }
   
 	_handleSubmit(e) {
 	  e.preventDefault();
 	  this.setState({
 	
-		addresss: ''
+		address: this.state.address,
+    vendor:this.state.vendor,
+    address1:this.state.address1,
+    quantity:this.state.quantity,
 	  });
+	  
   
 	  $.ajax({
 		url:"http://trackandt-Blockcha-OKH6MW7VYGQP-166143064.us-east-1.elb.amazonaws.com/batch",
 		type: 'POST',
 		data: {
-			Quantity: $("#quantity").val(),
-			Distributor: $("#distributor").val(),
+			"Product":routers,
+			"Category":"Network",
+			Quantity: this.state.quantity,
+			Distributor: this.state.vendor,
+			"Address": this.state.address1,
 	 	   'Upgrade device compatiblity to 5G': $("#upgrade").val(),
-		   'Delivery Address': this.state.addresss
+		   'Delivery Address': this.state.address
 		},
 		cache: false,
 		success: function(data) {
@@ -64,21 +82,42 @@ class Landingpage extends React.Component {
 		  // Success..
 		  var id=JSON.stringify(data);
 		  console.log('success', data);
-
+		  var name=localStorage.getItem('name');
+		  console.log(localStorage.getItem('name'));
+			//var assign=localStorage.getItem('name');
 		  $.ajax({
 			url:"https://pf1g1lmjel.execute-api.us-east-1.amazonaws.com/dev/createorder",
 			type: 'POST',
 			mode :'no-cors',
 			data: JSON.stringify({
 				
-				orderDetails: [JSON.stringify({"Product":"name","Category":"","Distributor": $("#distributor").val(),"Quantity :": $("#quantity").val(),"Upgrade to 5G":$("#upgrade").val(),"Delivery Address":this.state.address})],serviceProvider:"b",Distributor: $("#distributor").val(),transactionID:id
+				OrderDetails: [JSON.stringify({"Product":routers,"Category":"Network","Distributor":this.state.vendor,"Address": this.state.address1,"Quantity": this.state.quantity,"Upgradeto5G":$("#upgrade").val(),"DeliveryAddress":this.state.address})],ServiceProvider:name,Distributor:this.state.vendor,TransactionID:id
 			}),
 			cache: false,
 			success: function(data) {
 			  // Success..
 			 
-			  toast.success("Order is placed successfully!");
+			 
 			  console.log('success', data);
+			 var o=data;
+			
+			
+				var c=JSON.parse(o.OrderDetails.S);
+			// console.log('success', c.Product);
+			 
+			 localStorage.setItem('orderid',data.OrderID.S);
+			 localStorage.setItem('createdat',data.CreatedAt.S);
+			 localStorage.setItem('orderstatus',data.OrderStatus.S);
+			 localStorage.setItem('transactionid',data.TransactionID.S);
+			 localStorage.setItem('distributor',data.Distributor.S);
+			 localStorage.setItem('distributoraddress', c.Address);
+			 localStorage.setItem('product', c.Product);
+			 localStorage.setItem('category', c.Category);
+			 localStorage.setItem('quantity', c.Quantity);
+			 localStorage.setItem('deliveryaddress', c.DeliveryAddress);
+			 localStorage.setItem('upgrade', c.Upgradeto5G);
+		      
+			 toast.success("Order is placed successfully!");
 		
 			}.bind(this),
 			// Fail..
@@ -94,9 +133,8 @@ class Landingpage extends React.Component {
 			}.bind(this)
 		  });
 
-		  window.location="/#/orderdetails";
-		  toast.success("Order is placed successfully!");
-		  window.alert("Order is placed successfully!");
+		  
+		 
 		}.bind(this),
 		// Fail..
 		error: function(xhr, status, err) {
@@ -107,10 +145,11 @@ class Landingpage extends React.Component {
 			addresss: '<h1>Order could not be placed</h1><p>Try again later</p>'
 		  });
 		  console.log(this.state.addresss + 'fail');
+
 		}.bind(this)
 	  });
 	}
-  
+	
 	render() {
 	  return (
 		<div class="container-fluid padding0">
@@ -118,58 +157,47 @@ class Landingpage extends React.Component {
               <span className="logo"><img className="logoImage" src={logo} alt="Brillio logo" width="125px"/></span>
             
               <div className="userBlock collapse navbar-collapse">
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-      <DropdownToggle caret>
-			<span>
-		Welcome &nbsp;
-			<img src={image} className ="img-circle" alt={usericon} width="40" height="40"/>		
-			</span>
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem header>Options</DropdownItem>
-        <DropdownItem><Link to="/help"><button type="style" className="btn btn-block btn-primary">Help</button></Link></DropdownItem>
-        {/*<DropdownItem divider />*/}
-        <DropdownItem><GoogleLogout render={renderProps => (
-				<Link to="/login"><button type="style" className="btn btn-block btn-primary" onClick={renderProps.onClick}>Logout</button></Link>)}
-        /></DropdownItem>
-        </DropdownMenu>
-    </Dropdown>
+                <Link to="/help">Help</Link>&nbsp;<span className="pipe">|</span>&nbsp;<img src={usericon} alt="user" />
+	      	<span className="pipe">&nbsp;|&nbsp;</span>
+                <span>
+                {/*<GoogleLogouts/>*/}
+                <GoogleLogout render={renderProps => (
+                <Link to="/login"><span className="glyphicon glyphicon-log-out" onClick={renderProps.onClick}> Log Out </span>
+                </Link>)}
+                />
+                </span>
           	</div>               
           </header>
-	<section class="content">
+		  <section class="content">
 			<h3 class="section-header">Place Order</h3>
 			<div class="col-lg-12 col-md-12 place-order">
-				<div class="padding-bottom20">
-					<img src={router} alt="device" class="rect" width="100" height="100"/>
-				<h2>	<p class="network-device">Asus ROG Rapture GT-AC5300<br/><span>network</span></p></h2>
+			<div class="padding-bottom20">
+				
+				<h2>&nbsp;&nbsp;<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{routers}<br/></p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Network</span></h2>
 				</div>
 	
 			<form class="form-horizontal" onSubmit={this._handleSubmit} id="formContact">
 			<div class="col-lg-7 col-md-7">
-			<legend>Company Details</legend>
+			
 				
 				<div class="form-group col-md-6">
-					<label class="form-label">Vendor Name</label>
-					<select class="form-control" id="distributor">
-					<option>Abbott Group Enterprise</option>
-							<option default>Hirthe Group Enterprise</option>
-							<option>Schuster Ltd Enterprise</option>
-							</select>
-					
+					<label class="form-label"><b>Vendor Name : </b></label>
+					<input type="text" rows="8" cols="40" value={this.state.vendor} onChange={this._handleChangename} required/>				
 				</div>
+               
 				<div class="form-group col-md-6">
-					<label class="form-label">Quantity</label>
-					<select class="form-control"  id="quantity">
-						<option default>1000</option>
-						<option>2000</option>
-						<option>3000</option>
-						<option>4000</option>
-					</select>
+					<label class="form-label"><b>Address :</b></label>
+					<input type="text" rows="8" cols="40" value={this.state.address1} onChange={this._handleChange} required/>					
 				</div>
-				<br />
+			
+                <div class="form-group col-md-6">
+					<label class="form-label"><b>Quantity :</b></label>&nbsp;&nbsp;&nbsp;&nbsp;
+					<input type="text" rows="8" cols="40" value={this.state.quantity} onChange={this._handleChangeq} required/>					
+				</div>
+				<br/><br/><br/><br/><br/><br/>
 			  
 				<div class="form-group col-md-6">
-					<label class="form-label">Upgrade device compatiblity to 5G :</label>
+            		<label class="form-label"><b>Upgrade device compatiblity to 5G :</b></label>
 					<select class="form-control" id="upgrade">
 						<option default>Yes</option>
 						<option>No</option>
@@ -177,15 +205,16 @@ class Landingpage extends React.Component {
 				</div>
 				<br />
 			 
-						<div class="form-group col-md-12">
-							<label class="form-label">Delivery Address :</label><br/>
-							<textarea id="formMsg" name="formMsg" rows="8" cols="40" value={this.state.addresss} onChange={this._handleChangeMsg} required></textarea>
-						</div>
+                <div class="form-group col-md-12">
+                    <label class="form-label"><b>Delivery Address :</b></label><br/>
+                    <textarea id="formMsg" name="formMsg" rows="8" cols="40" value={this.state.address} onChange={this._handleChangeMsg} required></textarea>
+                 </div>
 
 
 			  <div class="col-lg-12 col-md-12 text-right">
-				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>
+				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>&nbsp;&nbsp;&nbsp;
 				<input type="submit" value="Submit" className="btn btn-cancel" id="btn-submit" />
+				
 				<ToastContainer />
 				</div></div>
 			</form>
