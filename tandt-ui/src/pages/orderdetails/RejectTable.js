d804a8876b4fc3e21fccffb99dfe49b399790d29
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-01-29 12:23:47 +0530

    Update RejectTable.js

--- tandt-ui/src/pages/orderdetails/RejectTable.js
+++ tandt-ui/src/pages/orderdetails/RejectTable.js
@@ -7,14 +7,27 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
 
 const useStyles = makeStyles({table: { minWidth: 650,},});
 
 export default function RejectTable(props) {
-  //const[disabled,setdisabled]=useState(false)
   const classes = useStyles();
   const {rowsess} = props;
+  // const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   return (
+    <Paper>
     <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
@@ -24,7 +37,7 @@ export default function RejectTable(props) {
               </TableRow>
           </TableHead>
           <TableBody>
-            {rowsess.map(row => (
+          {props.rowsess.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row =>  (      
               <TableRow key={row.id}>
                 <TableCell component="th" scope="row">{row.orderid}</TableCell>                
                 <TableCell>{row.orderstatus}</TableCell>
@@ -33,5 +46,15 @@ export default function RejectTable(props) {
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 7]}
+        component="div"
+        count={[10]}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+      </Paper>
   );
 }
