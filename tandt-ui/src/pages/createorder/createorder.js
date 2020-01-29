da900b28765d3e5d30fa2ac5e19dcd2d57c06f11
KavyaShreeKN <57769525+KavyaShreeKN@users.noreply.github.com>
2020-01-29 11:08:07 +0530

    Update createorder.js

--- tandt-ui/src/pages/createorder/createorder.js
+++ tandt-ui/src/pages/createorder/createorder.js
@@ -5,53 +5,86 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import logo from "./images/brillio-logo.png";
 import usericon from "./images/user-icon.svg";
-import router from "./images/router.png";
 import {GoogleLogin,GoogleLogout} from 'react-google-login';
 import "./dashboard.scss";
-import order from "../trackorder/trackorder";
-import $ from 'jquery';
+import $ from "jquery";
+var dateFormat = require('dateformat');
+
 var image=localStorage.getItem('profile-picture');
 var name=localStorage.getItem('name');
 var routers=localStorage.getItem('router');
+
 class Landingpage extends React.Component {
 	constructor(props){
 	  super(props);
 	  this.state = {
-		
 		quantity:'',
 		details: {},
-		
+		loading:false,
 	  };
-
-	 
 	  this._handleSubmit = this._handleSubmit.bind(this);
-
       this._handleChangeq = this._handleChangeq.bind(this);
 	}
-  
-	// Change state of input field so text is updated while typing
+	componentDidMount(){
+		let date_ob = new Date();
+		let date = ("0" + date_ob.getDate()).slice(-2);
+		let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+		let year = date_ob.getFullYear();
+		
+		let day=date+"/"+month+"/"+year;
+	   
+		
+		let hours = date_ob.getHours();
+		let minutes = date_ob.getMinutes();
+		let seconds = date_ob.getSeconds();
+		
+		let time=hours+":"+minutes+":"+seconds;
+	  
+		let dates=day+" "+time;
+		console.log(dates);
 
+		
+		
+
+	}
+	// Change state of input field so text is updated while typing
 	  _handleChangeq(e) {
 		  this.setState({
 			quantity: e.target.value,
 		  });
 		}
-	
-
-  
 	_handleSubmit(e) {
+		
 	  e.preventDefault();
 	  this.setState({
-	    quantity:this.state.quantity,
+		quantity:this.state.quantity,
+		loading:true,
 	  });
+	  let date_ob = new Date();
+	  let date = ("0" + date_ob.getDate()).slice(-2);
+	  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+	  let year = date_ob.getFullYear();
+	  
+	  let day=date+"/"+month+"/"+year;
+	 
+	  
+	  let hours = date_ob.getHours();
+	  let minutes = date_ob.getMinutes();
+	  let seconds = date_ob.getSeconds();
+	  
+	  let time=hours+":"+minutes+":"+seconds;
+	
+	  let dates=day+" "+time;
+	  console.log(dates);
 
-
-	  const data = { 		
-    "Product":routers,
+	const data = { 	
+		
+     "Product":routers,
 	  "Category":"Network",
 	  Quantity: this.state.quantity,
 	  "Manufacturer": $("#manufacture").val(),
 	  'Upgrade device compatiblity to 5G': $("#upgrade").val(),
+	  "Date":dates,
 	};
 
 	  fetch('http://trackandt-Blockcha-OKH6MW7VYGQP-166143064.us-east-1.elb.amazonaws.com/batch', {
@@ -65,9 +98,11 @@ class Landingpage extends React.Component {
 	  .then((data) => {
 			console.log('Success:', data.transactionId);
 			var id=JSON.stringify(data.transactionId);
+			var name=localStorage.getItem('name');
+			//var day=dateFormat(new Date(), "yyyy-mm-dd");
 			const data2={
 				
-				OrderDetails: [JSON.stringify({"Product":routers,"Category":"Network","Manufacturer":$("#manufacture").val(),"Quantity": this.state.quantity,"Upgradeto5G":$("#upgrade").val()})],ServiceProvider:name,Manufacturer:$("#manufacture").val(),TransactionID:id
+				Product:routers,Category:"Network",Quantity: this.state.quantity,Upgradeto5G:$("#upgrade").val(),ServiceProvider:name,Manufacturer:$("#manufacture").val(),TransactionID:id
 								
 			  };
 
@@ -76,7 +111,6 @@ class Landingpage extends React.Component {
 					headers: {
 		 			 'Content-Type': 'application/json',
 						},
-									
 					body: JSON.stringify(data2),
 					})
 	 		 .then((response) => response.json())
@@ -97,15 +131,15 @@ class Landingpage extends React.Component {
 	}
 	
 	render() { 
-		
+	
 	  return (
 		<div class="container-fluid padding0">
-<header>
+        <header>
               <span className="logo"><img className="logoImage" src={logo} alt="Brillio logo" width="125px"/></span>
             
               <div className="userBlock collapse navbar-collapse">
                 <Link to="/help">Help</Link>&nbsp;<span className="pipe">|</span>&nbsp;<img src={usericon} alt="user" />
-	      	<span className="pipe">&nbsp;|&nbsp;</span>
+	         	<span className="pipe">&nbsp;|&nbsp;</span>
                 <span>
                 {/*<GoogleLogouts/>*/}
                 <GoogleLogout render={renderProps => (
@@ -120,50 +154,44 @@ class Landingpage extends React.Component {
 			<h3 class="section-header">Place Order</h3>
 			<div class="col-lg-12 col-md-12 place-order">
 			<div class="padding-bottom20">
-			<div>
-
-    </div>
-				
-				<h2>&nbsp;&nbsp;<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{routers}<br/></p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Network</span></h2>
-				</div>
-				<div>
-  
-    </div>
+			<div><h2><b>{routers}</b></h2><br/></div>
+			</div>
+			<div class="padding-bottom20">
+			<div><b>Network Category</b><br/></div>
+			</div>
 	
 			<form class="form-horizontal" onSubmit={this._handleSubmit} id="formContact">
 			<div class="col-lg-7 col-md-7">
-			<div class="form-group col-md-6">
-							<label class="form-label"><b>Manufacturer</b></label>
-							
-							<select class="form-control" id="manufacture">
-									<option>Abbott Group Enterprise, Oklahoma, USA</option>
-									<option default>Hirthe Group Enterprise, Washington, USA</option>
-									<option>Schuster Ltd Enterprise, Alabama, USA</option>
-								</select>
-						</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<div class="form-group col-md-8" >
+			<div class="padding-bottom20">
+				<label class="form-label"><b>Manufacturer : </b></label>
+				<select class="form-control" id="manufacture">
+					<option>Abbott Group Enterprise, Oklahoma, USA</option>
+					<option default>Hirthe Group Enterprise, Washington, USA</option>
+					<option>Schuster Ltd Enterprise, Alabama, USA</option>
+				</select>
+			</div>
 			
-                <div class="form-group col-md-6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<label class="form-label"><b>Quantity :</b></label>
-					<input type="number" rows="8" cols="40" value={this.state.quantity} onChange={this._handleChangeq} required/>					
-				</div>
-				<br/><br/><br/><br/><br/><br/>
-			  
-				<div class="form-group col-md-6">
-            		<label class="form-label"><b>Upgrade device compatiblity to 5G :</b></label>
-					<select class="form-control" id="upgrade">
-						<option default>Yes</option>
-						<option>No</option>
-					</select>
-				</div>
-				<br />
-			 
-            
-			  <div class="col-lg-12 col-md-12 text-right">
+			<div class="form-group col-md-8" >
+				<label class="form-label"><b>Product Quantity : </b></label>
+				<input type="number"  min="1" max="99999" maxLength="5" class="form-control" value={this.state.quantity} onChange={this._handleChangeq} required/>					
+			
+            	<label class="form-label"><b>Upgrade device compatiblity to 5G :</b></label>
+				<select class="form-control" id="upgrade">
+					<option default>Yes</option>
+					<option>No</option>
+				</select>
+			</div></div>
+		
+			
+		    <div class="col-lg-12 col-md-12 text-right">
 				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>&nbsp;&nbsp;&nbsp;
-				<input type="submit" value="Submit" className="btn btn-cancel" id="btn-submit" />
-				</div></div>
+				<input type="submit" value="Submit" className="btn btn-prim" id="btn-submit" disabled={this.state.loading}></input>
+			</div>
+			</div>
 			</form>
-		  </div></section>
+		  </div>
+		  </section>
 		</div>
 	  )
 	}
