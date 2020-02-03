c996e591e7da224ee215bfc94c5708b05a4ce0d5
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-02-03 10:48:59 +0530

    Update OrderDetailsUI.js

--- tandt-ui/src/pages/orderdetails/OrderDetailsUI.js
+++ tandt-ui/src/pages/orderdetails/OrderDetailsUI.js
@@ -20,6 +20,7 @@ import CenteredTabs from './Tabs/CenteredTabs'
 import trackorder from "../trackorder/trackorder";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import Logout from '../login/Logout';
 //import DataTable, { createTheme } from 'react-data-table-component';
 //import {DataTable} from 'primereact/datatable';
 var orderid=[];
@@ -202,31 +203,7 @@ class OrderID extends React.Component{
 			return(			
 			<React.Fragment>	
 			<div className="container-fluid padding0">		
-      		<header>
-      		<span className="logo"><img className="logoImage" src={logo} alt="Brillio logo" width="125px"/>
-      		</span>				
-      		<div className="userBlock collapse navbar-collapse">	
-	  		<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-      		<DropdownToggle caret>
-				<span>
-				Welcome &nbsp;
-				<img src={localStorage.getItem('profile-picture')} className ="img-circle" alt={usericon} width="40" height="40"/>		
-				</span>
-      		</DropdownToggle>
-      			<DropdownMenu>
-        		<DropdownItem header>Options</DropdownItem>
-        		<DropdownItem><Link to="/help">
-				<button type="style" className="btn btn-block btn-primary">Help</button></Link></DropdownItem>
-        		<DropdownItem><GoogleLogout render={renderProps => (
-			<Link to="/login">
-			<button type="style"
-			className="btn btn-block btn-primary"
-			onClick={()=>{renderProps.onClick(); localStorage.removeItem('role')}}>Logout</button></Link>)}/>
-			</DropdownItem>
-       		</DropdownMenu>
-       		</Dropdown>
-			</div>				
-        	</header>				
+      			<Logout/>				
 			<section class="content">
 			{display1}
 			<br/>	
