03ebfe8d207c74da62971adaeca03ecefa2366c6
arshpreetkaur13 <57769615+arshpreetkaur13@users.noreply.github.com>
2020-02-04 10:59:57 +0530

    Update createorder.js

--- tandt-ui/src/pages/createorder/createorder.js
+++ tandt-ui/src/pages/createorder/createorder.js
@@ -7,9 +7,8 @@ import logo from "./images/brillio-logo.png";
 import usericon from "./images/user-icon.svg";
 import {GoogleLogin,GoogleLogout} from 'react-google-login';
 import "./dashboard.scss";
-import $ from "jquery";
 import Logout from '../login/Logout';
-
+import $ from "jquery";
 var image=localStorage.getItem('profile-picture');
 var name=localStorage.getItem('name');
 var routers=localStorage.getItem('router');
@@ -26,28 +25,7 @@ class Landingpage extends React.Component {
 	  this._handleSubmit = this._handleSubmit.bind(this);
       this._handleChangeq = this._handleChangeq.bind(this);
 	}
-	componentDidMount(){
-		let date_ob = new Date();
-		let date = ("0" + date_ob.getDate()).slice(-2);
-		let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-		let year = date_ob.getFullYear();
-		
-		let day=date+"/"+month+"/"+year;
-	   
-		
-		let hours = date_ob.getHours();
-		let minutes = date_ob.getMinutes();
-		let seconds = date_ob.getSeconds();
-		
-		let time=hours+":"+minutes+":"+seconds;
-	  
-		let dates=day+" "+time;
-		console.log(dates);
-
-		
-		
-
-	}
+	
 	// Change state of input field so text is updated while typing
 	  _handleChangeq(e) {
 		  this.setState({
@@ -104,12 +82,8 @@ class Landingpage extends React.Component {
 			//var day=dateFormat(new Date(), "yyyy-mm-dd");
 			const data2={
 				
-				Product:routers,Category:"Network",Quantity:this.state.quantity,Upgradeto5G:$("#upgrade").val(),ServiceProvider:name,Manufacturer:$("#manufacture").val(),TransactionID:data.transactionId,CreatedAt:Date(),OrderStatus:"Order Initiated",
-					
-				BatchCreatedOn:" ",
-				OrderShipped:" ",
-				OrderCompletedOn:" ",
-
+				Product:routers,Category:"Network",Quantity: this.state.quantity,Upgradeto5G:$("#upgrade").val(),ServiceProvider:name,Manufacturer:$("#manufacture").val(),TransactionID:id,CreatedAt:Date(),OrderStatus:"Order Initiated"
+								
 			  };
 
 			fetch('https://pf1g1lmjel.execute-api.us-east-1.amazonaws.com/dev/createorder', {
@@ -137,6 +111,7 @@ class Landingpage extends React.Component {
 	}
 	
 	render() { 
+		let p=this.props.location.state;
 		const { isPlacingOrder } = this.state;
 	  return (
 		<div class="container-fluid padding0">
@@ -146,7 +121,7 @@ class Landingpage extends React.Component {
 			<h3 class="section-header">Place Order</h3>
 			<div class="col-lg-12 col-md-12 place-order">
 			<div class="padding-bottom20">
-			<div><h2><b>{routers}</b></h2><br/></div>
+			<div><h2><b>{p}</b></h2><br/></div>
 			</div>
 			<div class="padding-bottom20">
 			<div><b>Network Category</b><br/></div>
@@ -158,9 +133,9 @@ class Landingpage extends React.Component {
 			<div class="padding-bottom20">
 				<label class="form-label"><b>Manufacturer : </b></label>
 				<select class="form-control" id="manufacture">
-					<option>Abbott Group Enterprise</option>
-					<option default>Hirthe Group Enterprise</option>
-					<option>Schuster Ltd Enterprise</option>
+					<option>Abbott Group Enterprise, Oklahoma, USA</option>
+					<option default>Hirthe Group Enterprise, Washington, USA</option>
+					<option>Schuster Ltd Enterprise, Alabama, USA</option>
 				</select>
 			</div>
 			
@@ -177,10 +152,9 @@ class Landingpage extends React.Component {
 		
 			
 		    <div class="col-lg-12 col-md-12 text-right">
-				
-				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>
-				<button class="btn btn-prim" type="Submit" id="btn-submit" disabled={this.state.loading}>{isPlacingOrder ? "Placing Order...": "Submit"}</button>
-
+				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>&nbsp;&nbsp;&nbsp;
+				{/* <input type="submit" value="Submit" className="btn btn-prim" id="btn-submit" disabled={this.state.loading}></input>*/}
+				<button class="btn btn-prim" type="Submit" id="btn-submit" disabled={this.state.loading}>{isPlacingOrder ? "Placing Order...": "Submit"}</button> 
 			</div>
 			</div>
 			</form>
