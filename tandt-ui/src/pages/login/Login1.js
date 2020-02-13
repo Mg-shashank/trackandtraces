7cb026b79b51b57c50d4aaf5c3a4497bd159a740
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-02-14 05:26:53 +0530

    Update Login1.js

--- tandt-ui/src/pages/login/Login1.js
+++ tandt-ui/src/pages/login/Login1.js
@@ -15,6 +15,12 @@ import image1 from './image1.png';
 import './site.scss';
 import orderdetails from '../orderdetails/orderdetails';
 import Unknown from './Unknown';
+import ttConfig from '../../config.js'
+import OrderID from '../orderdetails/OrderDetailsUI'
+// import config from 'react-global-configuration';
+var role, name;
+var email;
+let signout;
 
 const tstyle = {
     width:200,
@@ -32,13 +38,10 @@ const tstyle = {
   }
    const introimg = {
      height:'100vh',
-     width : '58vw',
-    backgroundRepeat: 'no-repeat',
-       backgroundSize: 'cover'
+     width : '55vw',
+     backgroundRepeat: 'no-repeat',
+     backgroundSize: 'cover'
     }
-    
- var email;
- let signout;
 
 class App extends React.Component {
   constructor() {
@@ -48,51 +51,80 @@ class App extends React.Component {
       isUserLoggedIn: false
     };
   }
-
+  
   responseGoogle = response => {
-    this.setState({ userDetails: response.profileObj, isUserLoggedIn: true });
+      console.log(this.state.isUserLoggedIn)
+      this.setState({ userDetails: response.profileObj});
+      console.log(this.state.isUserLoggedIn)
+    if(this.state.userDetails != undefined){
+      name  = this.state.userDetails.givenName
+      email = this.state.userDetails.email
+      this.setState({ isUserLoggedIn:true})
+      console.log(this.state.isUserLoggedIn)
+    }
   };
 
-  
- /* getRoleByEmailId(emailid) {
-    return roles.filter(function(roles){return (roles.emailId == emailid)});
-  }*/
- /* logout = () => {
-    this.setState({isUserLoggedIn: false})
-  };*/ 
+  redirectToOptions(){
+    console.log('REDIRECT TO OPTIONS')
+    if(this.state.isUserLoggedIn){   
+      if(role === 'serviceprovider'){ 
+        return <Redirect to="/dashboard" />   
+      }
+      else if((role === 'distributor')||(role === 'manufacturer')){ 
+        var refreshs = localStorage.setItem('refresh',null);
+        refreshs = localStorage.getItem('refresh');
+        console.log(refreshs);
+        if (refreshs === null){
+          console.log("REFRESH 1")
+          window.location.reload(false);
+          localStorage.setItem('refresh', "1");
+        }
+          return <Redirect to="/orderdetails" />  
+       }
+    }
+  }
 
-   render() {    
-        var name=this.state.userDetails.givenName;
-        //var surname=this.state.userDetails.familyName;
-        var email=this.state.userDetails.email;  
-        var image=this.state.userDetails.imageUrl;      
-        var loggedInUsersEmailId = email;
-        localStorage.setItem('name', name); 
-        localStorage.setItem('logged-in-user-email-id', loggedInUsersEmailId);
-        localStorage.setItem('profile-picture',image);
+  render() { 
+       ttConfig.roleassign.assign.name = name  
         
-        if(email==="shashank30051997@gmail.com"&&"4mc15cs050@gmail.com"){            
-            console.log(email);
-            localStorage.setItem('role', 'service provider');            
-        }  
-        else if(email==="abcd1@gmail.com"){            
-            console.log(email);
-            localStorage.setItem('role','ditributor' ); 
+        if( email === ttConfig.roleassign.serv.email ){ 
+          // role = config.get()  
+           role = ttConfig.roleassign.serv.role;
+           name = ttConfig.roleassign.serv.name; 
+           email = ttConfig.roleassign.serv.email;
+           console.log(name)
+           console.log(role)
+           localStorage.setItem('role',role)
+           localStorage.setItem('name',name)
+           }  
+        else if( email === ttConfig.roleassign.dist.email ){ 
+            role = ttConfig.roleassign.dist.role;
+            name = ttConfig.roleassign.dist.name;  
+            email = ttConfig.roleassign.dist.email;
+            console.log(name)  
+            console.log(role)
+            localStorage.setItem('role',role)
+            localStorage.setItem('name',name)
            }
-        else if(email=="abcd@gmail.com"){            
-            console.log(email);
-            localStorage.setItem('role','manufacturer' );             
-            }
-    
-    return (
-      <div className="App">      
+        else if( email === ttConfig.roleassign.manu.email ){  
+            role = ttConfig.roleassign.manu.role;
+            name = ttConfig.roleassign.manu.name;  
+            email = ttConfig.roleassign.manu.email;
+            console.log(name) 
+            console.log(role)     
+            localStorage.setItem('role',role)
+            localStorage.setItem('name',name)
+           }
+               
+    return (  
+        <div className="App">      
         {!this.state.isUserLoggedIn && (
         <div className="container-fluid padding0">
     		<div className="col-lg-7 col-md-7 padding0 introimg">    			
           <img src={brillio1} alt='image1' style={introimg}/>
-          </div>
-    		<div className="col-lg-5 col-md-5 login-container">
-        <div className="text-center" style={{fontSize:'30px'}}>WELCOME TO TRACK AND TRACE </div>
+        </div>
+    		<div className="col-lg-5 login-container">
+        <div className="text-center" style={{fontSize:'30px',fontFamily:'Arial'}}> WELCOME TO TRACK AND TRACE</div>
     			<p className="text-center"><img src={login} alt="login" /></p>
     			<div className="text-center">
             <div className="col-md-5">
@@ -100,16 +132,19 @@ class App extends React.Component {
             clientId="252338212303-eku5e56140e59uhvqeq36fl8neetega2.apps.googleusercontent.com" //TO BE CREATED
             render={renderProps => (       
               <div className="text-center" style={{width:"260px"}}>       
-              <button type="button" className="btn btn-primary btn-lg btn-block" 
-              onClick={renderProps.onClick} disabled={renderProps.disabled}> 
+              <button 
+              type="button" 
+              className="btn btn-primary btn-lg btn-block" 
+              onClick={renderProps.onClick} 
+              disabled={renderProps.disabled}> 
               Login 
-            </button>  
-            </div>
+              </button>  
+              </div>
             )}                           
-            onSuccess={ this.responseGoogle}
-            onFailure={this.responseGoogle}
+              onSuccess={ this.responseGoogle}
+              onFailure={this.responseGoogle}
             />
-          </div>{/*****************/}
+          </div>
           </div>
     	</div>
     	</div>          
@@ -120,9 +155,8 @@ class App extends React.Component {
              <GoogleLogout 
               render={renderProps => ( 
                  <div>
-                <Redirect to="/dashboard"/>
-              <button className="logout-button" onClick={renderProps.onClick}>Log Out </button>
-              </div>)}
+                {this.redirectToOptions()}
+                </div>)}
               onLogoutSuccess={this.logout}/>         
             </div>           
             </div>                
@@ -131,5 +165,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 export default App;
