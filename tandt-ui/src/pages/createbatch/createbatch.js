6f7988bbf52eb1906f83ca60843af8532f6ba85d
arshpreetkaur13 <57769615+arshpreetkaur13@users.noreply.github.com>
2020-02-14 10:31:04 +0530

    Update createbatch.js

--- tandt-ui/src/pages/createbatch/createbatch.js
+++ tandt-ui/src/pages/createbatch/createbatch.js
@@ -12,6 +12,7 @@ import Logout from '../login/Logout';
 import $ from "jquery";
 var image=localStorage.getItem('profile-picture');
 var name=localStorage.getItem('name');
+var id;
 
 class CreateBatch extends React.Component {
 	constructor(props){
@@ -21,29 +22,10 @@ class CreateBatch extends React.Component {
         details: {},
         loading:false,
 	  };
-	  this._handleSubmit = this._handleSubmit.bind(this);
-      this._handleChangeq = this._handleChangeq.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this)    
+    this._handleChangeq = this._handleChangeq.bind(this);
 	}
-	componentDidMount(){
-        let date_ob = new Date();
-		let date = ("0" + date_ob.getDate()).slice(-2);
-		let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-		let year = date_ob.getFullYear();
-		
-		let day=date+"/"+month+"/"+year;
-	   
-		
-		let hours = date_ob.getHours();
-		let minutes = date_ob.getMinutes();
-		let seconds = date_ob.getSeconds();
-		
-		let time=hours+":"+minutes+":"+seconds;
-	  
-		let dates=day+" "+time;
-		console.log(dates);
-        // console.log('debug search value',this.props.location.search.split('=')[1])
-        // var orderid = this.props.location.search.split('=')[1];        
-	}
+
 	// Change state of input field so text is updated while typing
 	  _handleChangeq(e) {
 		  this.setState({
@@ -51,58 +33,83 @@ class CreateBatch extends React.Component {
 		  });
 		}
 	_handleSubmit(e) {
-		
+    var OrderID=this.props.location.state;
+    var Quantity= this.props.location.state;
+
 	  e.preventDefault();
 	  this.setState({
-		batchquantity:this.state.batchquantity,
+    batchquantity:this.state.batchquantity,    
 		loading:true,
-	  });
-	  let date_ob = new Date();
-	  let date = ("0" + date_ob.getDate()).slice(-2);
-	  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-	  let year = date_ob.getFullYear();
-	  
-	  let day=date+"/"+month+"/"+year;
-	 
-	  
-	  let hours = date_ob.getHours();
-	  let minutes = date_ob.getMinutes();
-	  let seconds = date_ob.getSeconds();
-	  
-	  let time=hours+":"+minutes+":"+seconds;
-	
-	  let dates=day+" "+time;
-	  console.log(dates);
+    });
+    
+    var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
+    usaTime = new Date(usaTime);
+    var time=usaTime.toLocaleString()	
 
 	const data = { 	
-       OrderID:this.props.location.state,
-       BatchQuantity: this.state.batchquantity,      
-    //    Distributor: $("#distributor").val(), 
+      
+    BatchQuantity: this.state.batchquantity,      
+    Distributor: $("#distributor").val(), 
+    BatchCreatedOn: time,
     };
     console.log(data)
-
-    fetch('https://hscx60zx1c.execute-api.us-east-1.amazonaws.com/prod/entries1', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
+        fetch('http://trackandt-Blockcha-10MS595TSQEZ6-1475584145.us-east-1.elb.amazonaws.com/batch', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
             },
-        body: JSON.stringify(data),  
-        })
-    .then((response) => response.json())
-    .then((data2) => {
-        console.log('Success:', data2);
-    let details = data2;
-    let id = this.props.location.state;
-    this.props.history.push({pathname:'/batchdetails',state:details})
-        })
-    .catch((error) => {
-        console.error('Error:', error);
-        });
+            body: JSON.stringify(data),
+          })
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data)
+                console.log('Success:', data.transactionId);
+                id=JSON.stringify(data.transactionId);
+                var name=localStorage.getItem('name');
+                    const data2 = { 	
+                    OrderID:OrderID,
+                    Quantity: Quantity,
+                    OrderStatus:'Order Accepted By Manufacturer',
+                    BatchQuantity: this.state.batchquantity,      
+                    Distributor: $("#distributor").val(), 
+                    TransactionID2: id,
+                    BatchStatus:"Batch Created but Not Accepted by Distributor",
+                    BatchCreatedOn: time
+                 };
+                 console.log(id)
+                 console.log(data2.TransactionID)
+                fetch('https://hscx60zx1c.execute-api.us-east-1.amazonaws.com/prod/entries1', {
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json',
+                            },
+                        body: JSON.stringify(data2),
+                        })
+                  .then((response) => response.json())
+                  .then((data2) => {
+                        console.log('Success:', data2);
+                    let details = data2;
+                    let id = this.props.location.state;
+                    this.props.history.push({pathname:'/batchdetails',state:details})
+                        })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                        });
+    
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
     }
 	
 	render() { 
-        console.log('OrderID', this.props.location.state);
-       var orderid = this.props.location.state;
+    console.log('Quantity',this.props.location.state)
+    var orderid = this.props.location.state.OrderID;
+    var quantity= this.props.location.state.Quantity;
+    console.log(quantity);
+    
+        console.log('data', this.props.location.state);
+
        console.log(orderid)
 	  return (
 		<div class="container-fluid padding0">
@@ -118,10 +125,12 @@ class CreateBatch extends React.Component {
 			<form class="form-horizontal" onSubmit={this._handleSubmit} id="formContact">
 			<div class="col-lg-7 col-md-7">
 			<div class="form-group col-md-8" >
+
             <div>
             <label class="form-label"><b>Order ID : </b></label>
-            <p style={{backgroundColor:'white'}}>{this.props.location.state}</p>
+            <p style={{backgroundColor:'white'}}>{orderid}</p>
             </div>
+
 			<div class="padding-bottom20">
             <label class="form-label"><b>Distributor : </b></label>
 				<select class="form-control" id="distributor">
@@ -130,16 +139,23 @@ class CreateBatch extends React.Component {
 					<option>Joshua Williams </option>
 				</select>
 			</div>
-			
+
+			<div class="form-group col-md-8" >
+				<label class="form-label"><b>Product Quantity : </b></label>
+        <p style={{backgroundColor:'white'}}>{quantity}</p>
+        {/* <input type="number" class="form-control" value={this.props.location.state}  />  */}
+         {/* <p>{this.props.location.state}</p> */}
+        </div>
+
 			<div class="form-group col-md-8" >
 				<label class="form-label"><b>Batch Quantity : </b></label>
-				<input type="number"  class="form-control" value={this.state.batchquantity} onChange={this._handleChangeq} required/>					
+				<input type="number"  class="form-control" value={this.state.Quantity}  required/>					
 			</div>
             </div>		
 			
 		    <div class="col-lg-12 col-md-12 text-right">
-				<Link to="/dashboard"><div class="btn btn-cancel">Cancel</div></Link>&nbsp;&nbsp;&nbsp;
-				<input type="submit" value="CreateBatch" className="btn btn-prim" id="btn-submit" disabled={this.state.loading}></input>
+				<Link to="/orderdetails"><div class="btn btn-cancel">Cancel</div></Link>&nbsp;&nbsp;&nbsp;
+				<input type="submit" value="Create Batch"  className="btn btn-prim" align="center" float="right" id="btn-submit" disabled={this.state.loading}></input>
 			</div>
 			</div>
 			</form>
