cc7b171397e7d5e0b90762aac0fd560ba4d9f684
arshpreetkaur13 <57769615+arshpreetkaur13@users.noreply.github.com>
2020-02-17 15:58:44 +0530

    Update dashboard.js

--- tandt-ui/src/pages/dashboard/dashboard.js
+++ tandt-ui/src/pages/dashboard/dashboard.js
@@ -7,11 +7,10 @@ import logo from "./images/brillio-logo.png";
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
 import usericon from "./images/user-icon.svg";
-//import dots from "./images/dots.svg";
 import router from "./images/router.png";
 import router2 from "./images/router2.jpg";
 import router3 from "./images/router3.jpg"
-import "./dashboard.scss";
+import "./dashboard.scss"; 
 import Logout from '../login/Logout';
 var https = require("https");
 var image = localStorage.getItem('profile-picture');
@@ -19,32 +18,24 @@ var name = localStorage.getItem('name');
 
 class Landingpage extends React.Component {
 constructor(props){
-super(props);
-this.toggle= this.toggle.bind(this);
-this.state = {
-dropdownOpen:false,
-anchorEl:null,
-isLoading: true,
-ordPlaced:'',
-ordAccept:'',
-ordRec: '',
-recAct:'',
-e:'',
-selectedProduct: '',
-createOrder:'',
-}; 
-}
+	
+		super(props);
+		this.toggle= this.toggle.bind(this);
+		this.state = {
+		dropdownOpen:false,
+		anchorEl:null,
+		isLoading: true,
+		ordPlaced:'',
+		ordAccept:'',
+		ordRec: '',
+		recAct:'',
+		selectedProduct: '',
+		createOrder:'',
+		}; 
+
+	}
 	
 		componentDidMount() {
-			// var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
-			// usaTime = new Date(usaTime);
-			// var time=usaTime.toLocaleString()
-			// console.log(time);
-		    // console.log(usaTime);
-			// var h=usaTime.getHours();
-			// var m=usaTime.getMinutes();
-			// var dates=h+":"+m
-			// console.log(dates);
 			
 		fetch('https://pf1g1lmjel.execute-api.us-east-1.amazonaws.com/dev/fetchordercount', {
 		method:'GET',
@@ -124,29 +115,33 @@ createOrder:'',
 			console.error('Error:', error);
 				  });
 		
-	fetch('https://pf1g1lmjel.execute-api.us-east-1.amazonaws.com/dev/qldb-data', {
-		method:'GET',
-		headers: {
-		'Content-Type':'application/json',
-				},
-		})
-		
-		.then((response) =>response.json())
-			.then((data) => {
-			console.log(JSON.parse(data));
-			 var op =JSON.parse(data.toString())
-			 var op2 = JSON.parse(op.body)
-			console.log(JSON.parse(op2));
-			this.setState({
-			e: data,
-			isLoading:false,
-			});
-			
-		 })
-		.catch((error) => {
-			console.error('Error:', error);
-		 });
-
+				  fetch('https://av7avhrl4e.execute-api.us-east-1.amazonaws.com/prod/iontojsondevicedata', {
+					method:'GET',
+					headers: {
+					'Content-Type':'application/json',
+							},
+					})			
+					.then((response) =>response.json()).then((data) => {
+						
+						var Result=data;	
+						console.log(Result);					
+						//var productData=JSON.parse(Result.body);
+						 console.log(typeof(Result));
+						 console.log(typeof(Result.toString()));						
+						// console.log(data.body);
+						 var productData =(data.toString())
+						 var Result = (productData.body)					
+						var a = (data.body)
+						console.log(a);
+						// var Result = JSON.parse(productData.body)
+						//  	console.log(Result)
+						//  Result.forEach(element => {
+						// 	console.log(element);											
+						//  	});	
+					 })
+					.catch((error) => {
+						console.error('Error:', error);
+					 });
 
 	}
  	
@@ -180,9 +175,28 @@ createOrder:'',
 			this.props.history.push({pathname:'/createorder',state:details});
 		}
 	 };
- 
+
+	// createOrder(e) {
+		
+	// 	var selectedProduct = document.getElementsByClassName("selected");
+	// 	var details = selectedProduct[0].getAttribute('data-name')
+	// 	this.props.history.push({pathname:'/createorder',state:details});
+	//  };
+
+	gotoOrderPlace = () => {
+			this.props.history.push("/OrderPlaced");	
+	 		}
+
+	gotoOrderAcc= () => {		
+		this.props.history.push("/OrderAccepted");
+	}		 
+	
+	gotoOrderReceived = () => {
+		this.props.history.push("/OrderRec");
+	}
+
   	render(){	
-		const { isLoading,ordPlaced,ordAccept,ordRec,recAct,e} = this.state;
+		const { isLoading,ordPlaced,ordAccept,ordRec,recAct} = this.state;
       return( 
         <div className="wrapper">
          <Logout/>
@@ -193,33 +207,33 @@ createOrder:'',
 		<div className="col-lg-12 col-md-12">
 		<h3 className="section-header">Track Orders</h3>
 		</div>
-		<div className="col-lg-12 col-md-12 padding0">
-		<div className="col-lg-4 col-md-4">
-		<div className="track-order">
+		<div className="col-lg-12 col-md-12 padding0" >
+		<div className="col-lg-4 col-md-4" onClick={this.gotoOrderPlace} >
+		<div className="track-order" >
 		<h3>Order Placed</h3>
 		<p className="order-number"><div>
           {!isLoading ? ordPlaced: (
-            <p>Loading...</p>
+            <p>...</p>
           )}
         </div></p>
 		</div>
 		</div>
-		<div className="col-lg-4 col-md-4">
+		{/* <div className="col-lg-4 col-md-4" onClick={this.gotoOrderAcc}>
 		<div className="track-order">
 		<h3>Order Accepted</h3>
 		<p className="order-number"><div>
           {!isLoading ? ordAccept: (
-            <p>Loading...</p>
+            <p>...</p>
           )}
         </div></p>
 		</div>
-		</div>
-		<div className="col-lg-4 col-md-4">
+		</div> */}
+		<div className="col-lg-4 col-md-4"  onClick={this.gotoOrderReceived}>
 		<div className="track-order">
 		<h3>Completed Orders</h3>
 		<p className="order-number"><div>
           {!isLoading ? ordRec: (
-            <p>Loading...</p>
+            <p>...</p>
           )}
         </div></p>
 		</div>
@@ -227,21 +241,18 @@ createOrder:'',
 		</div>
 		<div className="row">
 		<div className="col-lg-12 col-lg-12">		
-		
-		</div>
-		<div className="col-lg-12 col-lg-12">		
-		
-		</div>
-		<div className="col-lg-12 col-lg-12">		
-		
-		<Link to="/createneworder"><button type="button" className="btn btn-primary pull-right" id="btn-create-order" >Create New Order</button></Link>
-		
-
+			{/* <button type="button" className="btn btn-prim pull-right" id="btn-create-order" onClick={this.createOrder.bind(this)}>Create New Order</button> &nbsp; */}
+			<div>
+			<div><span className="section-header"> &nbsp;&nbsp;&nbsp;Recommended Orders </span>
+			{/* <Link to="/createorder" onClick={this.createOrder.bind(this)}><button type="button" class="btn btn-primary pull-right" id="btn-create-order" disabled>+ Create New Order</button></Link> */}
+			<Link to="/createneworder"><button type="button" className="btn btn-primary pull-right" id="btn-create-order" >Create New Order</button></Link></div>
+			{/* <Link to="/completedrecords"><button type="button" className="btn btn-prim pull-left" id="btn-create-order">Previous Orders</button></Link> */}
+			</div>
 		</div>
 		</div>
 		</div>
 		</div>
-            
+	
 			<div className="col-lg-3 col-mt-3  float-right  recent-activities">
 			<div className="col-lg-12 col-mt-12 col-mt-5 padding0">
 			<h3 className="section-header">Recent Activities</h3>
@@ -250,124 +261,70 @@ createOrder:'',
 			<p className="act-head">Order Initiated</p>
 			<p className="act-content"><div>
           {!isLoading ? recAct : (
-            <p>Loading...</p>
+            <p>...</p>
           )}
         </div> is Initiated.</p>
-			</div>
-
-			<div className="activity-card">
-			<p className="act-head">Order Accepted </p>
-			<p className="act-content"><div>
-          {!isLoading ? recAct: (
-            <p>Loading...</p>
-          )}
-        </div> is accepted.</p>
+			{/* <p className="text-right"><Link to="/trackorder"><div className="view-more">View more</div></Link></p> */}
 			</div>
 		    </div>
-            </div>
-			<div><span className="section-header">&nbsp;&nbsp;&nbsp;Recommended Orders </span></div>
+            </div>			
+			
 			<div className="col-lg-9 col-md-9 padding0">
 			<div className="col-lg-4 col-md-4">
 			<div className="device-card" data-name="Asus ROG Rapture GT-AC5300" onClick={this.selectProductTile.bind(this)}>
-			
-			{/* <div className="dropdown dots"> */}
-			{/* <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
-			{/* <b>...</b> */}
-			{/* </Button>  */}
-			<Menu
-			id="simple-menu"
-			anchorEl={this.state.anchorEl}
-			keepMounted
-			open={Boolean(this.state.anchorEl)}s
-			// open={this.state.anchorEl}
-			onClose={this.handleClose}
-			>
-			<MenuItem onClick={this.handleClose}>Device Model: AC5300</MenuItem>
-			<MenuItem onClick={this.handleClose}>Device Type: Router</MenuItem>
-			<MenuItem onClick={this.handleClose}>Device Vendor: Asus</MenuItem>
-			</Menu>
-
-			
-			<img src={router} width="100" height="100" alt="s1" className="s1"  />	
-			{/* <div>
-          {!isLoading ?e: (
-            <p>Loading...</p>
-          )}
-        </div> */}
-		<ul>
-                    {Object.entries(e).map(([key, value])=>{
-                    return <li key={key}>{key}: {value}</li>
-                    })}
-                </ul> 
+						
+			<img src={router} width="100" height="100" alt="s1" className="s1"  />			
 			  <h3 className="title">Asus ROG Rapture GT-AC5300</h3>
 			  <p className="sub-title">Router</p>		
 			 <p className="card-label"><label>End of Warranty:</label> March</p>
-			<p className="attribute">MAC Address: D4:05:15:35:4A:11</p>
+			{/* <p className="attribute">MAC Address: D4:05:15:35:4A:11</p> */}
 			<p className="card-label"><label>You can include below parameters to make your device 5G- Ready:</label></p>
 			<p className="attribute">IPv6 Compatible: yes</p>
 			<p className="attribute">Data Transfer Rate: 1200 Mbps</p>
 			<p className="attribute">Frequency Band : Dual Band</p> 
+			<p className="attribute">Wireless specification:802.11ac </p>
+            <p className="attribute">Frequency: 2.4GHz and 5GHz </p>
 			</div>
 			</div>
 			<div className="col-lg-4 col-md-4">
 			<div className="device-card" data-name="D-Link AC2600 (DIR-2680)" onClick={this.selectProductTile.bind(this)}>						
-			
-			<Menu
-			id="simple-menu"
-			anchorEl={this.state.anchorEl}
-			keepMounted
-			//  open={Boolean(anchorEl)}
-			onClose={this.handleClose}
-			>
-			<MenuItem onClick={this.handleClose}>Device Model: AC2600</MenuItem>
-			<MenuItem onClick={this.handleClose}>Device Type: Router</MenuItem>
-			<MenuItem onClick={this.shandleClose}>Device Vendor: Abbot</MenuItem>
-			</Menu>                             
+			                          
 			
 			<img src={router2} alt="s1" width="100" height="100" className="s1" />
-			<h3 className="title">D-Link AC2600 (DIR-2680)</h3>
+			 <h3 className="title">D-Link AC2600 (DIR-2680)</h3>
 			<p className="sub-title">Router</p>
 		 	<p className="card-label"><label>End of Warranty:</label> June</p>
-			<p className="attribute">MAC Address: D2:65:75:87:A7:05</p>
+			{/* <p className="attribute">MAC Address: D2:65:75:87:A7:05</p> */}
 			<p className="card-label"><label>You can include below parameters to make your device 5G- Ready:</label></p>
 			<p className="attribute">IPv6 Compatible: yes</p>
 			<p className="attribute">Data Transfer Rate: 3600 Mbps</p>
 			<p className="attribute">Frequency Band : TriBand</p> 		 
+			<p className="attribute">Wireless specification:802.11ac </p>
+            <p className="attribute">Frequency: 2.4GHz and 5GHz </p>
 			</div>
 			</div>
 			<div className="col-lg-4 col-md-4">
 			<div className="device-card" data-name="Asus RT-AC66U B1" onClick={this.selectProductTile.bind(this)}>	
-
-			<Menu
-			id="simple-menu"
-			anchorEl={this.state.anchorEl}
-			keepMounted
-			//   open={Boolean(anchorEl)}
-			onClose={this.handleClose}
-			>
-			<MenuItem onClick={this.handleClose}>Device Model: AC66U</MenuItem>
-			<MenuItem onClick={this.handleClose}>Device Type: Router</MenuItem>
-			<MenuItem onClick={this.handleClose}>Device Vendor: Asus</MenuItem>
-			</Menu>
 			
 			<img src={router3} width="100" height="100" alt="s1" className="s1" />
 			 <h3 className="title">Asus RT-AC66U B1</h3>
 			<p className="sub-title">Router</p>
 			<p className="card-label"><label>End of Warranty:</label> June</p>
-			<p className="attribute">MAC Address: A4:02:33:93:7A:83</p>
+			{/* <p className="attribute">MAC Address: A4:02:33:93:7A:83</p> */}
 			<p className="card-label"><label>You can include below parameters to make your device 5G- Ready:</label></p>
 			<p className="attribute">IPv6 Compatible: yes</p>
 			<p className="attribute">Data Transfer Rate: 4600 Mbps</p>
 			<p className="attribute">Frequency Band : Dual Band</p>		
+			<p className="attribute">Wireless specification:802.11ac </p>
+            <p className="attribute">Frequency: 2.4GHz and 5GHz </p>
 			</div>
 			</div>
 			</div>
 			</div>
 			</section>  
 			</div>  
-
 	   );
 	}
-     }
+ }
 
      export default (Landingpage);
