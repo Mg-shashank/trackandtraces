fe3f31e04d9b2194e4aa34de28f9d4e9d643e4f2
Mg-shashank <57784703+Mg-shashank@users.noreply.github.com>
2020-02-19 15:52:18 +0530

    Update config.js

--- tandt-ui/src/config.js
+++ tandt-ui/src/config.js
@@ -14,17 +14,17 @@ const localConfig = {
   // continuousUpdate,
   tokens: {}
 }
-// parties.map(p => localConfig.tokens[p] = createToken(p));
 
-// Track and Trace config
- // config.set({
 const ttConfig = { 
   roleassign:{
     dist  :{role:'distributor'    ,name:'Steve'   ,email:'steve05022020@gmail.com'},
     manu  :{role:'manufacturer'   ,name:'Joe'     ,email:'joe015254@gmail.com'},
     serv  :{role:'serviceprovider',name:'Williams',email:'williams6733@gmail.com'},
-    assign:{role:'qwe', name:'qwe', email:'qwe'}
+    assign:{role:'qwe', name:'qwe', email:'qwe'},  
+    dcoordinates: {lat: 35.0078,lng: -97.0929 , lat1:36.0015 ,lng1:-77.5804},
+    mcoordinates: {lat: 35.5175, lng: -86.5804 ,lat1:35.0078 ,lng1:-97.0929 },
+    initialCenter: {lat:39.648209,lng:-65.711185}
    },
   }
-const config = ttConfig;
+ const config = ttConfig;
 export default config;
